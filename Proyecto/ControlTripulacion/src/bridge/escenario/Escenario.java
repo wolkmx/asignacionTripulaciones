f6e71d787fea312df21202a611bb72da3e808197
Roberto Hernandez De La Luz <robertohdll@gmail.com>
2017-05-28 16:23:40 -0500

    Esqueleto de clases para bridge.prototype

---
+++ Proyecto/ControlTripulacion/src/bridge/escenario/Escenario.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bridge.escenario;
+
+import bridge.UsuarioCondiciones;
+
+/**
+ *
+ * @author Roberto Hernandez De La Luz <www.ohsioh.com>
+ */
+public class Escenario implements UsuarioCondiciones{
+    
+}
