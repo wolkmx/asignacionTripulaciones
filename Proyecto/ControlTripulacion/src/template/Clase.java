9955b6ad097013187d4e2982d16a18b0586bd7f2
marcial235 <marcial2005@gmail.com>
2017-05-28 10:13:03 -0500

    Avance 1

---
+++ Proyecto/ControlTripulacion/src/template/Clase.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package template;
+
+/**
+ *
+ * @author Cristian Enríquez
+ */
+public class Clase {
+    //Eliminar, la creé solo para que se creara el directorio en git
+}
