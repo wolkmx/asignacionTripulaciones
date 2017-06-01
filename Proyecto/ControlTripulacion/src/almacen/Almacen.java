b59c3395409b8adc114f1b0bb42f1b652a0c5378
marcial235 <marcial2005@gmail.com>
2017-05-31 22:55:27 -0500

    UIPilots.java: -Cambio en el scrollpanel -y listVuelos (JTextArea  -> JTextPane) -Funcionalidad al boton "Buscar Vuelos". -Se crea la variable de clase nPiloto -Se agrega y da funcionalidad del boton "Consultar Escenarios". -Se agrega y da funcionalidad del boton "Realizar Simulación".
    Almacen.java:
    -Se crea el metodo getVuelosPiloto
    -Se crea el metodo getEscenarios
    
    ControlTripulacion.java:
    -Se agrega el metodo asignaTripulacionAVuelo
    
    Otros:
    -Se agrega imagen escenario.png
    -Se agrega imagen simulacion.png

--- Proyecto/ControlTripulacion/src/almacen/Almacen.java
+++ Proyecto/ControlTripulacion/src/almacen/Almacen.java
@@ -29,171 +29,232 @@ import registro.RegistroVuelos;
 /**
  *
  * @author Roberto Hernandez De La Luz <www.ohsioh.com>
- * Clase utileria para guardar las listas estaticas es decir como si fuera nuestra base de datos
+ * Clase utileria para guardar las listas estaticas es decir como si fuera
+ * nuestra base de datos
  */
 public class Almacen {
-    
-    public static List<RegistroPruebasSimulacion> registroPruebasSimulacion= new ArrayList<>();
-    public static Map<Integer,Aeropuerto> aeropuertos=new HashMap<Integer,Aeropuerto>();
+
+    public static List<RegistroPruebasSimulacion> registroPruebasSimulacion = new ArrayList<>();
+    public static Map<Integer, Aeropuerto> aeropuertos = new HashMap<>();
     public static Escenario escenario1 = new Escenario1();
     public static Escenario escenario2 = new Escenario2();
     public static Escenario escenario3 = new Escenario3();
     public static Escenario escenario4 = new Escenario4();
     public static Escenario escenario5 = new Escenario5();
     public static Escenario escenario6 = new Escenario6();
-    
-    public static Map<Integer,RegistroVuelos> registroVuelos= new HashMap<Integer,RegistroVuelos>();
-    
+
+    public static Map<Integer, RegistroVuelos> registroVuelos = new HashMap<>();
+
     public static List<Piloto> PILOTOS = new ArrayList<>();
     public static List<TripulacionReal> TRIPULACIONES = new ArrayList<>();
     public static List<Vuelo> VUELOS = new ArrayList<>();
-    
-    public static PilotoProxy getPilotoDisponible(String nombre){
-        PilotoProxy piloto= new PilotoProxy(nombre);
-        
+
+    public static PilotoProxy getPilotoDisponible(String nombre) {
+        PilotoProxy piloto = new PilotoProxy(nombre);
+
         return piloto;
-        
+
     }
-    
-    public static Piloto getPiloto(String nombre){
+
+    public static Piloto getPiloto(String nombre) {
         Piloto piloto = null;
-        for(Piloto p: PILOTOS){
-            if(p.getNombre().compareTo(nombre) == 0){
+        for (Piloto p : PILOTOS) {
+            if (p.getNombre().compareTo(nombre) == 0) {
                 piloto = p;
             }
         }
         return piloto;
     }
-    
+
     //Metodo estatico para obtener las simulaciones de un piloto dado , segun su numeroDeEmpleado
-    public static String[] getSimulacionesPiloto(String numeroEmpleado){
+    public static String[] getSimulacionesPiloto(String numeroEmpleado) {
         List<String> respuesta = new ArrayList<>();
-        
-        
-        for(RegistroPruebasSimulacion r: registroPruebasSimulacion){
-            if(r.getPiloto() == null){
+
+        for (RegistroPruebasSimulacion r : registroPruebasSimulacion) {
+            if (r.getPiloto() == null) {
                 respuesta.add("");
-            }else{
-            
-                if(r.getPiloto().getNombre().compareTo(numeroEmpleado) == 0 ){
-                    respuesta.add(numeroEmpleado+"-"+r.getEscenario().getNombre());
-                }
-            
+            } else if (r.getPiloto().getNombre().compareTo(numeroEmpleado) == 0) {
+                respuesta.add(numeroEmpleado + "-" + r.getEscenario().getNombre());
             }
         }
-        
+
         String[] t = respuesta.toArray(new String[0]);
-        
+
         return t;
     }
+
     /**
      * Metodo public estatico para obtener las caracteristicas de los vueolos
+     *
      * @param nVuelo
+     * @return
      */
-    
-    public static String getCaracteristicasVuelo(String nVuelo){
+    public static String getCaracteristicasVuelo(String nVuelo) {
         String respuesta = "";
-        
-        for(Entry<Integer,RegistroVuelos> e: registroVuelos.entrySet()){
-            if(String.valueOf(e.getValue().getVuelo().getNumeroVuelo()).compareTo(nVuelo) == 0){
+
+        for (Entry<Integer, RegistroVuelos> e : registroVuelos.entrySet()) {
+            if (String.valueOf(e.getValue().getVuelo().getNumeroVuelo()).compareTo(nVuelo) == 0) {
 
                 String temp = obtenerDatosCondiciones(e.getValue().getVuelo().getMeteorologica());
-                respuesta = respuesta + "Configuracion Meteorologica: "+temp+"<br/>";
+                respuesta = respuesta + "Configuracion Meteorologica: " + temp + "<br/>";
                 respuesta = respuesta + "=================<br/>";
-                
+
                 temp = obtenerDatosCondiciones(e.getValue().getVuelo().getOrografica());
-                respuesta = respuesta + "Configuracion Orografica: <br/>"+temp+"<br/>";
+                respuesta = respuesta + "Configuracion Orografica: <br/>" + temp + "<br/>";
                 respuesta = respuesta + "=================<br/>";
 
                 temp = obtenerDatosCondiciones(e.getValue().getVuelo().getVisibilidad());
-                respuesta = respuesta + "Configuracion Visibilidad: "+temp+"<br/>";
+                respuesta = respuesta + "Configuracion Visibilidad: " + temp + "<br/>";
                 respuesta = respuesta + "=================<br/>";
-                
+
                 break;
             }
-            
+
         }
-        
-        
-        return "<html><body style='width:100%;'>"+respuesta+"</body></html>";
+
+        return "<html><body style='width:100%;'>" + respuesta + "</body></html>";
     }
-    
-    
-    public static RegistroVuelos getVuelo(String numero){
+
+    public static RegistroVuelos getVuelo(String numero) {
         RegistroVuelos rv = null;
-        
-        for(Entry<Integer,RegistroVuelos> e: registroVuelos.entrySet()){
-            if(String.valueOf(e.getValue().getVuelo().getNumeroVuelo()).compareTo(numero) == 0){
+
+        for (Entry<Integer, RegistroVuelos> e : registroVuelos.entrySet()) {
+            if (String.valueOf(e.getValue().getVuelo().getNumeroVuelo()).compareTo(numero) == 0) {
                 rv = e.getValue();
             }
         }
-        
+
         return rv;
     }
-    
+
     /**
-     * Metodo publico estatico para obtener un listado de tipo string con todos los numeros de vuelo existentes
-    */
-    public static String[] getVuelosExistentes(){
+     * Metodo publico estatico para obtener un listado de tipo string con todos
+     * los numeros de vuelo existentes
+     *
+     * @return
+     */
+    public static String[] getVuelosExistentes() {
         List<String> respuesta = new ArrayList<>();
-        
-        for(Entry<Integer,RegistroVuelos> e: registroVuelos.entrySet()){
+
+        for (Entry<Integer, RegistroVuelos> e : registroVuelos.entrySet()) {
             respuesta.add(String.valueOf(e.getValue().getVuelo().getNumeroVuelo()));
         }
-        
+
         String[] t = respuesta.toArray(new String[0]);
-        
+
         return t;
     }
-    
+
     /**
-     * Metodo publico estatico para obtener una cadena HTML con la informacion de las calificaicones de la simulacion
-    */
-    public static String getCalificacionSimulacion(String nombreEscenario){
+     * Metodo publico estatico para obtener una cadena HTML con la informacion
+     * de las calificaicones de la simulacion
+     *
+     * @param nombreEscenario
+     * @return
+     */
+    public static String getCalificacionSimulacion(String nombreEscenario) {
         String respuesta = "";
         String[] valores = nombreEscenario.split("-");
-        
-        for(RegistroPruebasSimulacion r: Almacen.registroPruebasSimulacion){
-            if(r.getPiloto() != null){
-                if((r.getPiloto().getNombre().compareTo(valores[0]) == 0) && (r.getEscenario().getNombre().compareTo(valores[1]) == 0) ){
-                    respuesta = "Escenario: "+r.getEscenario().getNombre()+"<br/>Calificaicon Meteorologica: "+r.getCalificacionMeteorologica()+"<br/>";
-                    respuesta = respuesta + "Calificacion Orografica: "+r.getCalificacionOrografica()+"<br/>";
-                    respuesta = respuesta + "Calificacion Visibilidad: "+r.getCalificacionVisibilidad()+"<br/>";
-                    respuesta = respuesta + "Fecha: "+r.getFecha()+"<br/>";
+
+        for (RegistroPruebasSimulacion r : Almacen.registroPruebasSimulacion) {
+            if (r.getPiloto() != null) {
+                if ((r.getPiloto().getNombre().compareTo(valores[0]) == 0) && (r.getEscenario().getNombre().compareTo(valores[1]) == 0)) {
+                    respuesta = "Escenario: " + r.getEscenario().getNombre() + "<br/>Calificaicon Meteorologica: " + r.getCalificacionMeteorologica() + "<br/>";
+                    respuesta = respuesta + "Calificacion Orografica: " + r.getCalificacionOrografica() + "<br/>";
+                    respuesta = respuesta + "Calificacion Visibilidad: " + r.getCalificacionVisibilidad() + "<br/>";
+                    respuesta = respuesta + "Fecha: " + r.getFecha() + "<br/>";
                     respuesta = respuesta + "=================<br/>";
-                    
+
                     String temp = obtenerDatosCondiciones(r.getEscenario().getMeteorologica());
-                    respuesta = respuesta + "Configuracion Meteorologica: "+temp+"<br/>";
+                    respuesta = respuesta + "Configuracion Meteorologica: " + temp + "<br/>";
                     respuesta = respuesta + "=================<br/>";
-                    
+
                     temp = obtenerDatosCondiciones(r.getEscenario().getOrografica());
-                    respuesta = respuesta + "Configuracion Orografica: <br/>"+temp+"<br/>";
+                    respuesta = respuesta + "Configuracion Orografica: <br/>" + temp + "<br/>";
                     respuesta = respuesta + "=================<br/>";
-                    
+
                     temp = obtenerDatosCondiciones(r.getEscenario().getVisibilidad());
-                    respuesta = respuesta + "Configuracion Visibilidad: "+temp+"<br/>";
+                    respuesta = respuesta + "Configuracion Visibilidad: " + temp + "<br/>";
                     respuesta = respuesta + "=================<br/>";
-                    
+
                 }
             }
 
         }
-        
-        
-        return "<html><body style='width:100%;'>"+respuesta+"</body></html>";
+
+        return "<html><body style='width:100%;'>" + respuesta + "</body></html>";
     }
-    
-    public static String obtenerDatosCondiciones(Condiciones[] condiciones){
+
+    public static String obtenerDatosCondiciones(Condiciones[] condiciones) {
         String respuesta = "";
-        for(Condiciones c: condiciones){
-            respuesta = respuesta + "Tipo: "+c.getClass().getSimpleName()+"<br/>";
-            for(String x : c.getInfo()){
-                respuesta = respuesta + x+"<br/>";
+        for (Condiciones c : condiciones) {
+            if (c != null) {
+                respuesta = respuesta + "Tipo: " + c.getClass().getSimpleName() + "<br/>";
+                for (String x : c.getInfo()) {
+                    respuesta = respuesta + x + "<br/>";
+                }
             }
         }
-        
+
         return respuesta;
     }
-    
-    
+
+    /**
+     * Método estatico para recuperar los vuelos que pertenecen a un piloto
+     *
+     * @param numeroEmpleado
+     * @return
+     */
+    public static String getVuelosPiloto(String numeroEmpleado) {
+        String respuesta = "";
+
+        for (Entry<Integer, RegistroVuelos> e : registroVuelos.entrySet()) {
+            TripulacionReal x = (TripulacionReal) e.getValue().getTripulacion();
+            if (String.valueOf(x.getTripulacion()[0].getnEmpleado()).compareTo(numeroEmpleado) == 0
+                    || String.valueOf(x.getTripulacion()[1].getnEmpleado()).compareTo(numeroEmpleado) == 0) {
+                respuesta = "Número de vuelo: " + String.valueOf(e.getValue().getVuelo().getNumeroVuelo()) + "<br>"
+                        + "Aeropuerto destino: " + e.getValue().getVuelo().getAeropuertoEntrada().getNombre() + "<br>"
+                        + "Aeropuerto salida: " + e.getValue().getVuelo().getAeropuertoSalida().getNombre() + "<br>"
+                        + "Condiciones meteorológicas previstas: <br>"
+                        + getCaracteristicasVuelo(String.valueOf(e.getValue().getVuelo().getNumeroVuelo()));;
+            } else {
+                respuesta = "No tiene vuelos asignados";
+            }
+        }
+        return respuesta;
+    }
+
+    /**
+     * Método estatico para recuperar los escenarios
+     */
+    public static String getEscenarios() {
+        String respuesta = "";
+        respuesta = "<b>Escenario 1: </b>" + escenario1.getNombre()+"<br>"
+                + obtenerDatosCondiciones(escenario1.getMeteorologica())+"<br>"
+                + obtenerDatosCondiciones(escenario1.getOrografica())+"<br>"
+                + obtenerDatosCondiciones(escenario1.getVisibilidad())+"<br>"
+                + "<b>Escenario 2: </b>" + escenario2.getNombre()+"<br>"
+                + obtenerDatosCondiciones(escenario2.getMeteorologica())+"<br>"
+                + obtenerDatosCondiciones(escenario2.getOrografica())+"<br>"
+                + obtenerDatosCondiciones(escenario2.getVisibilidad())+"<br>"
+                + "<b>Escenario 3: </b>" + escenario3.getNombre()+"<br>"
+                + obtenerDatosCondiciones(escenario3.getMeteorologica())+"<br>"
+                + obtenerDatosCondiciones(escenario3.getOrografica())+"<br>"
+                + obtenerDatosCondiciones(escenario3.getVisibilidad())+"<br>"
+                + "<b>Escenario 4: </b>" + escenario4.getNombre()+"<br>"
+                + obtenerDatosCondiciones(escenario4.getMeteorologica())+"<br>"
+                + obtenerDatosCondiciones(escenario4.getOrografica())+"<br>"
+                + obtenerDatosCondiciones(escenario4.getVisibilidad())+"<br>"
+                + "<b>Escenario 5: </b>" + escenario5.getNombre()+"<br>"
+                + obtenerDatosCondiciones(escenario5.getMeteorologica())+"<br>"
+                + obtenerDatosCondiciones(escenario5.getOrografica())+"<br>"
+                + obtenerDatosCondiciones(escenario5.getVisibilidad())+"<br>"
+                + "<b>Escenario 6: </b>" + escenario6.getNombre()+"<br>"
+                + obtenerDatosCondiciones(escenario6.getMeteorologica())+"<br>"
+                + obtenerDatosCondiciones(escenario6.getOrografica())+"<br>"
+                + obtenerDatosCondiciones(escenario6.getVisibilidad());
+
+        return respuesta;
+    }
+
 }
--- Proyecto/ControlTripulacion/src/main/ControlTripulacion.java
+++ Proyecto/ControlTripulacion/src/main/ControlTripulacion.java
@@ -5,18 +5,24 @@
  */
 package main;
 
+import almacen.Almacen;
 import static almacen.Almacen.PILOTOS;
 import static almacen.Almacen.VUELOS;
+import static almacen.Almacen.registroVuelos;
 import bridge.Aeropuerto;
 import bridge.Vuelo;
 import composite.proxy.Piloto;
+import composite.singleton.Tripulacion;
+import composite.singleton.TripulacionReal;
 import interfaces.SeleccionarUsuario;
+import registro.RegistroVuelos;
 
 /**
  *
  * @author Cristian Enríquez
  */
 public class ControlTripulacion {
+
     //Para crear pilotos desde el arranque
     public static void pilotos() {
         PILOTOS.add(new Piloto("Jon Dao", 001, 10, 360, "Piloto de prueba"));
@@ -25,19 +31,37 @@ public class ControlTripulacion {
         PILOTOS.add(new Piloto("Jin Dio", 004, 40, 260, "Piloto de prueba"));
         PILOTOS.add(new Piloto("Jun Duo", 005, 50, 160, "Piloto de prueba"));
     }
-    //Para crear un vuelo desde el inicio *Borrar cuando ya no se use*
-    public static void vuelos(){
-    Aeropuerto a1 = new Aeropuerto();
-    Aeropuerto a2 = new Aeropuerto();
-    
-    a1.setNombre("Aeropuerto Salida");
-    a2.setNombre("Aeropuerto destino");
-    
-    Vuelo v1 = new Vuelo();
-    v1.setAeropuertoEntrada(a2);
-    v1.setAeropuertoSalida(a1);
-    v1.setNumeroVuelo(23);
-    VUELOS.add(v1);
+
+    /*
+    *--> Creamos un vuelo
+    *--> Asignamos una tripulacion a ese vuelo
+    **/
+    public static void asignaTripulacionAVuelo() {
+
+        //Creamos los aeropuertos
+        Aeropuerto a1 = new Aeropuerto();
+        Aeropuerto a2 = new Aeropuerto();
+        a1.setNombre("Aeropuerto Salida");
+        a2.setNombre("Aeropuerto destino");
+
+        //Creamos el vuelo
+        Vuelo v1 = new Vuelo();
+        v1.setAeropuertoEntrada(a2);
+        v1.setAeropuertoSalida(a1);
+        v1.setNumeroVuelo(23);
+        VUELOS.add(v1);
+
+        //Asignamos tripulacion
+        TripulacionReal tripulacion = new TripulacionReal();
+        tripulacion.setTripulante(0, new Piloto("Piloto piloto", 10, 10, 360, "Piloto de prueba"));
+        tripulacion.setTripulante(1, new Piloto("Piloto copiloto", 20, 20, 360, "Copiloto de prueba"));
+        
+        //Registramos el vuelo
+        RegistroVuelos rv = new RegistroVuelos();
+        rv.setVuelo(v1);
+        rv.setTripulacion(tripulacion);
+        
+        registroVuelos.put(v1.getNumeroVuelo(), rv);
     }
 
     /**
@@ -47,7 +71,11 @@ public class ControlTripulacion {
         //Llenamos pilotos de prueba
         pilotos();
         //Vuelo de prueba
-        vuelos();
+        asignaTripulacionAVuelo();
+        
+        //Testeando
+        Almacen.getVuelosPiloto("20");
+        
         //Inicializa UI
         java.awt.EventQueue.invokeLater(() -> {
             SeleccionarUsuario seleccionarUsuario = new SeleccionarUsuario();
